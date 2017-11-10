989ba204a70245a8e8fe9a1675105ac2832a70b6
Piotr Milewski <pmilewski@stuy.edu>
2017-11-10 15:36:14 -0500

    work

--- main.c
+++ main.c
@@ -5,9 +5,24 @@
 
 #include "main.h"
 
+char **parse_args( char *line){
+  char retArr[8][64];
+  char **p = retArr;
+  int count = 0;
+  while (line){
+    p[count] = strsep( p, " ");
+    count++;
+  }
+  return p;
+}
+
 int main(){
 
-
+  char line[200];
+  fgets( line, sizeof(line), stdin);
+  
+  char **args = parse_args( line);
+  execvp( args[0], args);
 
   return 0;
 }
